155692a8fd7bc439360f7435dc10d5dffb562bfd
Alexander Boldt <alexx1216@live.de>
2021-08-11 17:26:03 +0200

    added limit-parameter for get-notes-route

--- src/main/kotlin/feature/Notes.kt
+++ src/main/kotlin/feature/Notes.kt
@@ -4,11 +4,11 @@ import com.alex.main.kotlin.repository.Note
 import com.alex.main.kotlin.repository.NotesList
 import com.alex.main.kotlin.repository.NotesRepository
 import io.ktor.application.call
-import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
-import io.ktor.request.receiveOrNull
 import io.ktor.response.respond
 import io.ktor.routing.*
+import com.alex.main.kotlin.repository.Error
+import io.ktor.request.*
 
 object Notes {
 
@@ -18,7 +18,8 @@ object Notes {
 
     fun routing(): Routing.() -> Unit = {
         get("notes") {
-            call.respond(HttpStatusCode.OK, NotesList(notesRepository.get()))
+            val limit = call.parameters["limit"]?.toIntOrNull()
+            call.respond(HttpStatusCode.OK, NotesList(notesRepository.getAll(limit)))
         }
 
         get("notes/{id}") {
@@ -29,11 +30,11 @@ object Notes {
                     notesRepository.get(id)?.apply {
                         call.respond(HttpStatusCode.OK, this)
                     } ?: run {
-                        call.respond(HttpStatusCode.BadRequest, com.alex.main.kotlin.repository.Error("Note not found with given id!"))
+                        call.respond(HttpStatusCode.BadRequest, Error("Note not found with given id!"))
                     }
                 }
                 ?: run {
-                    call.respond(HttpStatusCode.BadRequest, com.alex.main.kotlin.repository.Error("Invalid id!"))
+                    call.respond(HttpStatusCode.BadRequest, Error("Invalid id!"))
                 }
         }
 
@@ -42,10 +43,9 @@ object Notes {
                 .receiveOrNull<Note>()
                 ?.apply {
                     notesRepository.save(this)
-                    call.respond(HttpStatusCode.Created, NotesList(notesRepository.get()))
-                }
-                ?: run {
-                    call.respond(HttpStatusCode.BadRequest, com.alex.main.kotlin.repository.Error("Unexpected body-request"))
+                    call.respond(HttpStatusCode.Created, NotesList(notesRepository.getAll()))
+                } ?: run {
+                    call.respond(HttpStatusCode.BadRequest, Error("Unexpected body-request"))
                 }
         }
 
@@ -61,34 +61,35 @@ object Notes {
                                 if (isUpdated) {
                                     call.respond(HttpStatusCode.OK, notesRepository.get(id)!!)
                                 } else {
-                                    call.respond(HttpStatusCode.BadRequest, com.alex.main.kotlin.repository.Error("Could not update note!"))
+                                    call.respond(HttpStatusCode.BadRequest, Error("Could not update note!"))
                                 }
                             }
                         }
                         ?: run {
-                            call.respond(HttpStatusCode.BadRequest, com.alex.main.kotlin.repository.Error("Invalid id!"))
+                            call.respond(HttpStatusCode.BadRequest, Error("Invalid id!"))
                         }
                 }
                 ?: run {
-                    call.respond(HttpStatusCode.BadRequest, com.alex.main.kotlin.repository.Error("Unexpected body-request!"))
+                    call.respond(HttpStatusCode.BadRequest, Error("Unexpected body-request!"))
                 }
         }
 
+        // it's possible to delete a whole collection, but not desirable
         delete("notes") {
             when (notesRepository.delete()) {
-                true -> call.respond(HttpStatusCode.OK, NotesList(notesRepository.get()))
-                false -> call.respond(HttpStatusCode.Conflict, com.alex.main.kotlin.repository.Error("Could not delete all notes!"))
+                true -> call.respond(HttpStatusCode.OK, NotesList(notesRepository.getAll()))
+                false -> call.respond(HttpStatusCode.Conflict, Error("Could not delete all notes!"))
             }
         }
 
         delete("notes/{id}") {
             call.parameters["id"]?.toIntOrNull()?.apply {
                 when (notesRepository.delete(this)) {
-                    true -> call.respond(HttpStatusCode.OK, NotesList(notesRepository.get()))
-                    false -> call.respond(HttpStatusCode.Conflict, com.alex.main.kotlin.repository.Error("Could not delete note!"))
+                    true -> call.respond(HttpStatusCode.OK, NotesList(notesRepository.getAll()))
+                    false -> call.respond(HttpStatusCode.Conflict, Error("Could not delete note!"))
                 }
             } ?: run {
-                call.respond(HttpStatusCode.Conflict, com.alex.main.kotlin.repository.Error("Invalid id!"))
+                call.respond(HttpStatusCode.Conflict, Error("Invalid id!"))
             }
         }
     }
--- src/main/kotlin/repository/NotesRepository.kt
+++ src/main/kotlin/repository/NotesRepository.kt
@@ -6,11 +6,26 @@ class NotesRepository {
 
     // -----------------------------------------------------------------------------
 
-    fun get() = notes
-    fun get(id: Int) = notes.firstOrNull { it.id == id }
+    init {
+        // seeds
+        notes.addAll(listOf(
+            Note(1, "Einkaufen", "Das Gemüse nicht vergessen"),
+            Note(2, "Aufräumen", "Grundreinigung"),
+            Note(3, "Abwaschen", null),
+            Note(4, "Tanken", "Diesel"),
+            Note(4, "Blumen gießen", "Den Dünger nicht vergessen")))
+    }
 
+    // -----------------------------------------------------------------------------
+
+    // create
     fun save(note: Note) = notes.add(note.apply { id = notes.size + 1 })
 
+    // read
+    fun getAll(limit: Int? = null) = if (limit != null) notes.take(limit) else notes
+    fun get(id: Int) = notes.firstOrNull { it.id == id }
+
+    // update
     fun update(note: Note): Boolean {
         return notes
             .indexOfFirst { it.id == note.id }
@@ -18,6 +33,7 @@ class NotesRepository {
             .let { it != -1 }
     }
 
+    // delete
     fun delete(): Boolean {
         notes.clear()
         return true
