2fc5d70431c82eea2e7c4ac714e1682c11bfb4c9
Alexander Boldt <boldt.alexander@outlook.com>
2025-12-02 11:55:49 +0100

    created extensions for better data handling from requests

--- src/main/kotlin/org/alex/notes/feature/Notes.kt
+++ src/main/kotlin/org/alex/notes/feature/Notes.kt
@@ -1,61 +1,54 @@
 package org.alex.notes.feature
 
-import org.alex.notes.domain.NoteRequest
 import org.alex.notes.repository.NoteTable
 import io.ktor.http.HttpStatusCode
+import io.ktor.server.request.receive
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.put
-import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
+import io.ktor.server.routing.RoutingContext
 import io.ktor.server.routing.delete
 import io.ktor.server.routing.route
-import io.ktor.server.util.getOrFail
+import org.alex.notes.domain.NoteRequest
 import org.alex.notes.service.NoteService
 import org.alex.notes.utils.Path
-import org.alex.notes.utils.PathParam
-import org.alex.notes.utils.QueryParam
-import org.alex.notes.utils.convertToSort
+import org.alex.notes.utils.getSort
+import org.alex.notes.utils.id
 import org.koin.ktor.ext.inject
+
+suspend fun RoutingContext.getNote() = call.receive<NoteRequest>()
 
 fun Route.noteRoutes() {
     route(Path.NOTES) {
-
         val noteService: NoteService by inject()
 
         // create
 
         post {
-            val note = call.receive<NoteRequest>()
-            call.respond(HttpStatusCode.Created, noteService.create(note))
+            call.respond(HttpStatusCode.Created, noteService.create(getNote()))
         }
 
         // read
 
         get {
-            val sort = call.queryParameters[QueryParam.SORT]?.convertToSort(NoteTable.columns)
-            call.respond(noteService.readAll(sort))
+            call.respond(noteService.readAll(getSort(NoteTable.columns)))
         }
 
         get(Path.ID) {
-            val id = call.pathParameters.getOrFail<Int>(PathParam.ID)
             call.respond(noteService.read(id))
         }
 
         // update
 
         put(Path.ID) {
-            val id = call.pathParameters.getOrFail<Int>(PathParam.ID)
-            val note = call.receive<NoteRequest>()
-
-            call.respond(noteService.update(id, note))
+            call.respond(noteService.update(id, getNote()))
         }
 
         // delete
 
         delete(Path.ID) {
-            val id = call.pathParameters.getOrFail<Int>(PathParam.ID)
             noteService.delete(id)
 
             call.respond(HttpStatusCode.NoContent)
---
+++ src/main/kotlin/org/alex/notes/utils/RoutingExtensions.kt
@@ -0,0 +1,10 @@
+package org.alex.notes.utils
+
+import io.ktor.server.routing.RoutingContext
+import io.ktor.server.util.getOrFail
+import org.jetbrains.exposed.sql.Column
+
+val RoutingContext.id: Int
+    get() = call.pathParameters.getOrFail<Int>(PathParam.ID)
+
+fun RoutingContext.getSort(columns: List<Column<*>>) = call.queryParameters[QueryParam.SORT]?.convertToSort(columns)
