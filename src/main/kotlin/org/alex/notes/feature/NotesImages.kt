73dcf6dce21b450a2f900ac92cfbbe781b3ee2a8
Alexander Boldt <boldt.alexander@outlook.com>
2026-02-05 16:20:41 +0100

    created logic to download and delete an image from a note

--- src/main/kotlin/org/alex/notes/feature/NotesImages.kt
+++ src/main/kotlin/org/alex/notes/feature/NotesImages.kt
@@ -1,9 +1,16 @@
 package org.alex.notes.feature
 
+import io.ktor.http.ContentDisposition
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.request.receiveMultipart
+import io.ktor.server.response.header
 import io.ktor.server.response.respond
+import io.ktor.server.response.respondBytes
 import io.ktor.server.routing.post
 import io.ktor.server.routing.Route
+import io.ktor.server.routing.delete
+import io.ktor.server.routing.get
 import io.ktor.server.routing.route
 import org.alex.notes.service.NoteImageService
 import org.alex.notes.utils.Path
@@ -13,12 +20,29 @@ fun Route.noteImageRoutes() {
     route(Path.NOTES_IMAGES) {
         val noteImageService: NoteImageService by inject()
 
-        // create
-
         post {
             val multipartData = call.receiveMultipart(formFieldLimit = 1024 * 1024 * 100)
 
             call.respond(noteImageService.uploadImage(multipartData))
+        }
+
+        get {
+            val (stream, filename) = noteImageService.downloadImage()
+
+            call.response.header(
+                HttpHeaders.ContentDisposition,
+                ContentDisposition.Attachment
+                    .withParameter(ContentDisposition.Parameters.FileName, filename)
+                    .toString()
+            )
+
+            call.respondBytes(stream.readBytes())
+        }
+
+        delete {
+            noteImageService.deleteImage()
+
+            call.respond(HttpStatusCode.NoContent)
         }
     }
 }
