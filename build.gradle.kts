75ed64576b1c2052d40d2b2d9b2bcbb0afedf42b
Alexander Boldt <boldt.alexander@outlook.com>
2026-04-25 09:46:00 +0200

    added cors library

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ tasks.withType<Test> {
 dependencies {
     // development libraries
     implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.cors)
     implementation(libs.ktor.server.content.negotiation)
     implementation(libs.ktor.server.netty)
     implementation(libs.ktor.server.status.pages)
